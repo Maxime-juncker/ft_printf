8e7ae9e51c257cf30bd8797938dc8bc142ec280e
Maxime Juncker <maxime.junck@gmail.com>
2024-11-20 11:06:05 +0100

    added back libft but without submodule

---
+++ libft/src/ft_memmove.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjuncker <mjuncker@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 17:47:31 by mjuncker          #+#    #+#             */
+/*   Updated: 2024/11/13 16:52:46 by mjuncker         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*cdest;
+	unsigned char	*csrc;
+
+	if (dest == NULL && src == NULL && n == 0)
+		return (NULL);
+	cdest = (unsigned char *)dest;
+	csrc = (unsigned char *)src;
+	if (cdest < csrc)
+		ft_memcpy(dest, src, n);
+	else
+	{
+		cdest += n - 1;
+		csrc += n - 1;
+		while (n--)
+			*cdest-- = *csrc--;
+	}
+	return (dest);
+}
